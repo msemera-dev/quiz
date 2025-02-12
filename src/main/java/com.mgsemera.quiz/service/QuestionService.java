66d4418407cc3a92984e5290558ab89518529eca
msemera-dev <msemera.dev@gmail.com>
2025-02-12 23:07:04 +0800

    DEV-0001: Add Controller, Service, DAO for adding and getting questions

---
+++ src/main/java/com.mgsemera.quiz/service/QuestionService.java
@@ -0,0 +1,28 @@
+package com.mgsemera.quiz.service;
+
+import com.mgsemera.quiz.Question;
+import com.mgsemera.quiz.dao.QuestionDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    QuestionDao questionDao;
+
+    public List<Question> getAllQuestions() {
+        return questionDao.findAll();
+    }
+
+    public List<Question> getQuestionsByCategory(String category) {
+        return questionDao.findByCategory(category);
+    }
+
+    public String addQuestion(Question question) {
+        questionDao.save(question);
+        return "Successfully added question";
+    }
+}
